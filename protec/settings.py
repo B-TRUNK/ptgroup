812e9d0f40505403b9c08bfd032e8eccb4124272
B-TRUNK <abanob.boschra@gmail.com>
2026-08-16 15:26:26 +0300

    postgre-connect

--- protec/settings.py
+++ protec/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'protec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "protec",
+#         "USER": "root",
+#         "PASSWORD": "adel@pt26",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "protec",
-        "USER": "root",
-        "PASSWORD": "adel@pt26",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'protec',
+        'USER': 'postgres',
+        'PASSWORD': 'adel@pt26',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
