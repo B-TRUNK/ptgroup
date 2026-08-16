05b44335d2b41e2db90d4158ceca510881d74f03
B-TRUNK <abanob.boschra@gmail.com>
2026-08-16 22:13:48 +0300

    fix_allowed_hosts

--- protec/settings.py
+++ protec/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-+e%93srx4ps6fvrytm-a7w5q^8)c8t*d)$-jhv)6-r1c9@fa3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "ptgroup.vercel.app",
+]
 
 
 # Application definition
